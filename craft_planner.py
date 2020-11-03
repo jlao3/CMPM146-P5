dcaefa978f38a69ee5213adaa30b5d026cdb36a3
jlao3 <65569051+jlao3@users.noreply.github.com>
2020-11-03 13:36:05 -0800

    Did the simple functions

---
+++ craft_planner.py
@@ -0,0 +1,160 @@
+import json
+from collections import namedtuple, defaultdict, OrderedDict
+from timeit import default_timer as time
+from math import inf
+from heapq import heappop, heappush
+
+Recipe = namedtuple('Recipe', ['name', 'check', 'effect', 'cost'])
+
+
+class State(OrderedDict):
+    """ This class is a thin wrapper around an OrderedDict, which is simply a dictionary which keeps the order in
+        which elements are added (for consistent key-value pair comparisons). Here, we have provided functionality
+        for hashing, should you need to use a state as a key in another dictionary, e.g. distance[state] = 5. By
+        default, dictionaries are not hashable. Additionally, when the state is converted to a string, it removes
+        all items with quantity 0.
+
+        Use of this state representation is optional, should you prefer another.
+    """
+
+    def __key(self):
+        return tuple(self.items())
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __lt__(self, other):
+        return self.__key() < other.__key()
+
+    def copy(self):
+        new_state = State()
+        new_state.update(self)
+        return new_state
+
+    def __str__(self):
+        return str(dict(item for item in self.items() if item[1] > 0))
+
+
+def make_checker(rule):
+    # Implement a function that returns a function to determine whether a state meets a
+    # rule's requirements. This code runs once, when the rules are constructed before
+    # the search is attempted.
+
+    def check(state):
+        # This code is called by graph(state) and runs millions of times.
+        # Tip: Do something with rule['Consumes'] and rule['Requires'].
+        if 'Requires' in rule: # 'Requires' section of dictionary
+            for requirement in rule['Requires']: # For every requirement
+                if not state[requirement]: # If we don't have said requirement
+                    return False # Return False
+        if 'Consumes' in rule:  # 'Consumes' section of dictionary
+            for consumed in rule['Consumes']: # For every consumed
+                if not state[consumed] >= rule['Consumes'][consumed]: # If we don't have enough of consumable
+                    return False # Return False
+        return True # Else return true
+
+    return check # return check value
+
+
+def make_effector(rule):
+    # Implement a function that returns a function which transitions from state to
+    # new_state given the rule. This code runs once, when the rules are constructed
+    # before the search is attempted.
+
+    def effect(state):
+        # This code is called by graph(state) and runs millions of times
+        # Tip: Do something with rule['Produces'] and rule['Consumes'].
+        next_state = state.copy()
+        if 'Consumes' in rule: # 'Consumes' section of dictionary
+            for consumed in rule['Consumes']: # For every consumed
+                next_state[consumed] -= rule['Consumes'][consumed] # Decrease amount of current state by how much is used
+        if 'Produces' in rule: # 'Produces' section of dictionary
+            for product in rule['Produces']: # For every product produced
+                next_state[product] += rule['Produces'][product] # Increase current states amount by how much is made
+        return next_state # Return new_state
+
+    return effect # Return effect state
+
+
+def make_goal_checker(goal):
+    # Implement a function that returns a function which checks if the state has
+    # met the goal criteria. This code runs once, before the search is attempted.
+
+    def is_goal(state):
+        # This code is used in the search process and may be called millions of times.
+        for endgame in goal: # For every item in goal
+            if state[endgame] < goal[endgame]: # If current state contains less that goal value
+                return False # return false
+        return True # Else we have goal value, return true
+
+    return is_goal
+
+
+def graph(state):
+    # Iterates through all recipes/rules, checking which are valid in the given state.
+    # If a rule is valid, it returns the rule's name, the resulting state after application
+    # to the given state, and the cost for the rule.
+    for r in all_recipes: # UNCHANGED
+        if r.check(state):
+            yield (r.name, r.effect(state), r.cost)
+
+
+def heuristic(state):
+    # Implement your heuristic here!
+    return 0
+
+def search(graph, state, is_goal, limit, heuristic):
+
+    start_time = time()
+
+    # Implement your search here! Use your heuristic here!
+    # When you find a path to the goal return a list of tuples [(state, action)]
+    # representing the path. Each element (tuple) of the list represents a state
+    # in the path and the action that took you to this state
+    while time() - start_time < limit:
+        pass
+
+    # Failed to find a path
+    print(time() - start_time, 'seconds.')
+    print("Failed to find a path from", state, 'within time limit.')
+    return None
+
+if __name__ == '__main__':
+    with open('Crafting.json') as f:
+        Crafting = json.load(f)
+
+    # # List of items that can be in your inventory:
+    # print('All items:', Crafting['Items'])
+    #
+    # # List of items in your initial inventory with amounts:
+    # print('Initial inventory:', Crafting['Initial'])
+    #
+    # # List of items needed to be in your inventory at the end of the plan:
+    # print('Goal:',Crafting['Goal'])
+    #
+    # # Dict of crafting recipes (each is a dict):
+    # print('Example recipe:','craft stone_pickaxe at bench ->',Crafting['Recipes']['craft stone_pickaxe at bench'])
+
+    # Build rules
+    all_recipes = []
+    for name, rule in Crafting['Recipes'].items():
+        checker = make_checker(rule)
+        effector = make_effector(rule)
+        recipe = Recipe(name, checker, effector, rule['Time'])
+        all_recipes.append(recipe)
+
+    # Create a function which checks for the goal
+    is_goal = make_goal_checker(Crafting['Goal'])
+
+    # Initialize first state from initial inventory
+    state = State({key: 0 for key in Crafting['Items']})
+    state.update(Crafting['Initial'])
+
+    # Search for a solution
+    resulting_plan = search(graph, state, is_goal, 5, heuristic)
+
+    if resulting_plan:
+        # Print resulting plan
+        for state, action in resulting_plan:
+            print('\t',state)
+            print(action)
